e663cbaabca24cc0e5c1cfb69f52eb16154b92de
3teamsocial <3teamsocial@gmail.com>
2025-12-28 21:14:30 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6B4EFF",
+        dark: "#1F1F1F"
+      }
+    },
+  },
+  plugins: [],
+}
